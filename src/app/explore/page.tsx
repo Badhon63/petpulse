0012dd5abe21d0fe4937ce168c349a26c852a7b9
Badhon63 <jianrobiul7898@gmail.com>
2026-07-14 13:11:22 +0600

    fixed deployment related issue

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -1,5 +1,6 @@
 import { getProducts } from "@/lib/fetch";
 import Explore from "./Explore";
+export const dynamic = "force-dynamic";
 
 export default async function ExplorePage() {
   const products = await getProducts();
